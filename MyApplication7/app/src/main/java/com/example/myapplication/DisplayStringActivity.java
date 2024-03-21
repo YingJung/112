fdd08eae734aad9049595d654c850d9d4affcd5f
YingJung <a111221035@mail.shu.edu.tw>
2024-03-21 17:59:23 +0800

    3.21

---
+++ MyApplication7/app/src/main/java/com/example/myapplication/DisplayStringActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class DisplayStringActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_display_string);
+
+        TextView txvShow = (TextView) findViewById(R.id.txvshow);
+        Intent intent = getIntent();
+        String outputString = intent.getStringExtra("userName");
+        txvShow.setText(receivedString);
+    }
+}
